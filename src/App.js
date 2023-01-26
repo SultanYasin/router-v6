b5dd7364191f0b17b166f31bb34800ad5c7a5f03
Sultan Yasin <sultan.yasin@utb.ecutbildning.se>
2023-01-26 22:46:56 +0100

    router v6

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
-
-import './App.css';
-import {createBrowserRouter, createRoutesFromElements, Route, Outlet, Link, RouterProvider} from 'react-router-dom'
-import Home from './components/Home';
-import Component1 from './components/Component1';
-import Component2 from './components/Component2';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import {FetchedData, dataLoader} from './components/FetchedData';
+import "./App.css";
+import { createBrowserRouter, createRoutesFromElements, Route, Outlet, Link, RouterProvider,
+} from "react-router-dom";
+import Home from "./components/Home";
+import Component1 from "./components/Component1";
+import Component2 from "./components/Component2";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { FetchedData, dataLoader } from "./components/FetchedData";
 
 function App(props) {
-
-const costumeRouter = createBrowserRouter(
-  createRoutesFromElements(
-
-    <Route path='/' element={<Root />} >
-      <Route index element = {<Home />}   />
-      <Route path='/component1' element = {<Component1 />}   />
-      <Route path='/component2' element = {<Component2 />}   />
-      <Route path='/header'   element = {<Header />}   />
-      <Route path='/footer'  element = {<Footer />}   />
-      <Route path='/fetchdata'  element = {<FetchedData />} loader={dataLoader}  />
-
-    </Route>
-
-  )
-)
+  const costumeRouter = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path="/component1" element={<Component1 />} />
+        <Route path="/component2" element={<Component2 />} />
+        <Route path="/header" element={<Header />} />
+        <Route path="/footer" element={<Footer />} />
+        <Route
+          path="/fetchdata"
+          element={<FetchedData />}
+          loader={dataLoader}
+        />
+      </Route>
+    )
+  );
 
   return (
     <div className="App">
-
       <RouterProvider router={costumeRouter} />
-     
     </div>
   );
 }
@@ -38,17 +36,28 @@ const costumeRouter = createBrowserRouter(
 export default App;
 
 const Root = () => {
-  return <> 
-  <div> 
-    <Link to="/" >Home</Link>
-    <Link to="/component1" >Component1</Link>
-    <Link to="/component2" >Component2</Link>
-    <Link to="/header" >Header</Link>
-    <Link to="/footer" >Footer</Link>
-    <Link to="/fetchdata" >FetchData</Link>
-     </div> 
-     <div>
-      <Outlet />
-     </div>
-     </>
-}
+  return (
+    <>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/component1">Component1</Link>
+        <Link to="/component2">Component2</Link>
+        <Link to="/header">Header</Link>
+        <Link to="/footer">Footer</Link>
+        <Link to="/fetchdata">FetchData</Link>
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+};
+
+/*
+ createBrowserRouter()
+ inside of the createBrowserRouter() bring another method -> createRoutesFromElements() ->
+createBrowserRouter(createRouterFromElements(
+  <Route path="/" element={<Root />}  /> in this function I define all links that I wanna to navigate to -> const root =()=>{ <Link to = "" /> <Link to = "" /> <div> <outlet /></otlet> </div> }
+  <Route index element={<Home />}  /> first element has to be the landing page
+))
+ */
